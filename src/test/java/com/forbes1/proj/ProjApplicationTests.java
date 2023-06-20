8d8c965d101489f981ea7afca0d9ce32d993cb92
Ndedi Georges Patrick Nkollo (DHL DE), external <n.patrick_nkollo@deutschepost.de>
2023-06-20 22:27:13 +0200

    @SpringBootTest(classes = ProjApplicationTests.class) in test class

--- src/test/java/com/forbes1/proj/ProjApplicationTests.java
+++ src/test/java/com/forbes1/proj/ProjApplicationTests.java
@@ -3,7 +3,7 @@ package com.forbes1.proj;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = ProjApplicationTests.class)
 class ProjApplicationTests {
 
 	@Test
